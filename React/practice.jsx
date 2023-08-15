9cc5c84d54b19529029f8981f5dbd7cb3375fceb
daryle <daryle.tan@gmail.com>
2023-08-14 19:37:12 -0700

    create class and rendered

--- React/practice.jsx
+++ React/practice.jsx
@@ -166,6 +166,22 @@ class TypesOfFood extends React.Component {
   }
 };
 
-// Change code below this line
 ReactDOM.render(<TypesOfFood />, document.getElementById('challenge-node'))
+*/
+
+/* Create class and render it 
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div>
+        <h1>My First React Component!</h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<MyComponent />, document.getElementById('challenge-node'))
 */
