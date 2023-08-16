6dc8a217bf9c4516703e1f51d88a68bb64b36553
daryle <daryle.tan@gmail.com>
2023-08-15 19:00:05 -0700

    14/20

--- React/practice.jsx
+++ React/practice.jsx
@@ -185,3 +185,29 @@ class MyComponent extends React.Component {
 
 ReactDOM.render(<MyComponent />, document.getElementById('challenge-node'))
 */
+
+/* prop down 
+const CurrentDate = (props) => {
+  return (
+    <div>
+      <p>The current date is: {props.date}</p>
+    </div>
+  );
+};
+
+class Calendar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h3>What date is it?</h3>
+        <CurrentDate date={Date()}/>
+      </div>
+    );
+  }
+};
+*/
+
+/* */
