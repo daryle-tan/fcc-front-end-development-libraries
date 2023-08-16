a29e29f99c67132a377ea0725ce3253b009f7673
daryle <daryle.tan@gmail.com>
2023-08-15 19:05:23 -0700

    15/20

--- React/practice.jsx
+++ React/practice.jsx
@@ -210,4 +210,28 @@ class Calendar extends React.Component {
 };
 */
 
-/* */
+/* Passing prop arrays with .join() 
+const List = (props) => {
+ 
+  return <p>{props.tasks.join(", ")}</p>
+
+};
+
+class ToDo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>To Do Lists</h1>
+        <h2>Today</h2>
+
+        <List tasks={["walk dog", "workout"]}/>
+        <h2>Tomorrow</h2>
+        <List tasks={["run", "lift", "code"]}/>
+      </div>
+    );
+  }
+};
+*/
