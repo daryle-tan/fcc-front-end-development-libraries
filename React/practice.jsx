73d144d25a3f4f54e6bb97dcab2ba9207891cdb1
daryle <daryle.tan@gmail.com>
2023-08-16 18:48:13 -0700

    override default

--- React/practice.jsx
+++ React/practice.jsx
@@ -249,3 +249,23 @@ ShoppingCart.defaultProps = {
   items: 0
 }
 */
+/* override default prop 
+const Items = (props) => {
+  return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+}
+
+Items.defaultProps = {
+  quantity: 0
+}
+
+class ShoppingCart extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+   
+    return <Items quantity={10}/>
+   
+  }
+};
+*/
