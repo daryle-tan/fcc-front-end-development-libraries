4b58c320b618633232b0b4539280c4014b0a9f64
daryle <daryle.tan@gmail.com>
2023-08-17 19:57:33 -0700

    render state using const

--- React/practice.jsx
+++ React/practice.jsx
@@ -387,3 +387,21 @@ class MyComponent extends React.Component {
   }
 };
 */
+/* render state in user interface 
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'freeCodeCamp'
+    }
+  }
+  render() {
+    const name = this.state.name;
+    return (
+      <div>
+        <h1>{name}</h1>
+      </div>
+    );
+  }
+};
+*/
