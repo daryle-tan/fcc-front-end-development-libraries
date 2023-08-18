a8808448f0d700516f65ff56e5a187c2e6dea0bd
daryle <daryle.tan@gmail.com>
2023-08-17 19:46:57 -0700

    rendering state

--- React/practice.jsx
+++ React/practice.jsx
@@ -367,3 +367,23 @@ class StatefulComponent extends React.Component {
   }
 };
 */
+
+/* rendering state 
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'freeCodeCamp'
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>
+        {this.state.name}
+        </h1>
+      </div>
+    );
+  }
+};
+*/
