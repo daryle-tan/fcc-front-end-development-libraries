c0049275528c4fcd702f11aae86073f454922511
daryle <daryle.tan@gmail.com>
2023-08-19 09:04:24 -0700

    32/47

--- React/practice.jsx
+++ React/practice.jsx
@@ -700,4 +700,19 @@ class RenderInput extends React.Component {
   }
 };
 */
+/* Lifecycle Methods ComponentWillMount
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentWillMount() {
+    // Change code below this line
+    console.log("Hola")
+    // Change code above this line
+  }
+  render() {
+    return <div />
+  }
+};
+*/
 /* */
