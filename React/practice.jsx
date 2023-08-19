4b43b441391b3281a0d8abe5352a535d5b484355
daryle <daryle.tan@gmail.com>
2023-08-19 09:10:23 -0700

    33/47

--- React/practice.jsx
+++ React/practice.jsx
@@ -715,4 +715,29 @@ class MyComponent extends React.Component {
   }
 };
 */
-/* */
+/* ComponentDidMount
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeUsers: null
+    };
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        activeUsers: 1273
+      });
+    }, 2500);
+  }
+  render() {
+    return (
+      <div>
+        {}
+        <h1>Active Users: {this.state.activeUsers}</h1>
+        {}
+      </div>
+    );
+  }
+}
+*/
