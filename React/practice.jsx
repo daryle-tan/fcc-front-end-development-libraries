5f411c28b5fbbc8dafe858ef2c2b83aa8622e8dc
daryle <daryle.tan@gmail.com>
2023-08-19 08:39:01 -0700

    30/47

--- React/practice.jsx
+++ React/practice.jsx
@@ -594,14 +594,50 @@ class MyForm extends React.Component {
           <button type='submit'>Submit!</button>
         </form>
         { 
-
         }
           <h1>{this.state.submit}</h1>
-        {
-          
+        {         
         }
       </div>
     );
   }
 }
+*/
+/* pass state as props to child
+class MyApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'CamperBot'
+    }
+  }
+  render() {
+    return (
+       <div>
+         {
+         }
+         <Navbar name={this.state.name}/>
+         {
+         }
+       </div>
+    );
+  }
+};
+
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+    <div>
+      { 
+      }
+      <h1>Hello, my name is: {this.props.name}</h1>        
+      {       
+      }
+    </div>
+    );
+  }
+};
 */
