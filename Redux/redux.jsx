aa385a5689dee0990028cfe2fd75f1c0009ab5bc
daryle <daryle.tan@gmail.com>
2023-08-24 18:37:43 -0700

    8/17

--- Redux/redux.jsx
+++ Redux/redux.jsx
@@ -134,5 +134,35 @@ const logoutUser = () => {
   }
 };
 
+// store.subscribe
+const ADD = 'ADD';
 
+const reducer = (state = 0, action) => {
+  switch(action.type) {
+    case ADD:
+      return state + 1;
+    default:
+      return state;
+  }
+};
+
+const store = Redux.createStore(reducer);
+
+// Global count variable:
+let count = 0;
+
+
+function incrementCount() {
+  count++;
+}
+store.subscribe(incrementCount)
+
+store.dispatch({type: ADD});
+console.log(count);
+store.dispatch({type: ADD});
+console.log(count);
+store.dispatch({type: ADD});
+console.log(count);
+
+//
  */
