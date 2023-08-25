7227929b50133cb3bfea1c2354db20503db0f12d
daryle <daryle.tan@gmail.com>
2023-08-24 18:33:00 -0700

    7/17

--- Redux/redux.jsx
+++ Redux/redux.jsx
@@ -93,5 +93,46 @@ const logoutUser = () => {
   }
 };
 
+// User const for action types
+const LOGIN = 'LOGIN';
+const LOGOUT = 'LOGOUT';
+
+const defaultState = {
+  authenticated: false
+};
+
+const authReducer = (state = defaultState, action) => {
+
+  switch (action.type) {
+    case LOGIN: 
+      return {
+        authenticated: true
+      }
+    case LOGOUT: 
+      return {
+        authenticated: false
+      }
+
+    default:
+      return state;
+
+  }
+
+};
+
+const store = Redux.createStore(authReducer);
+
+const loginUser = () => {
+  return {
+    type: LOGIN
+  }
+};
+
+const logoutUser = () => {
+  return {
+    type: LOGOUT
+  }
+};
+
 
  */
