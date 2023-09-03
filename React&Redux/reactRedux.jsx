07ecd78dcc60aa10189627f5780ff91b2b4e3db3
daryle <daryle.tan@gmail.com>
2023-09-02 21:03:50 -0700

    7/10

--- React&Redux/reactRedux.jsx
+++ React&Redux/reactRedux.jsx
@@ -192,5 +192,39 @@ const mapDispatchToProps = (dispatch, message) => {
 }
 
 // 7/10
+const addMessage = (message) => {
+  return {
+    type: 'ADD',
+    message: message
+  }
+};
+
+const mapStateToProps = (state) => {
+  return {
+    messages: state
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    submitNewMessage: (message) => {
+      dispatch(addMessage(message));
+    }
+  }
+};
+
+class Presentational extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <h3>This is a Presentational Component</h3>
+  }
+};
+
+const connect = ReactRedux.connect;
+const ConnectedComponent = connect(mapStateToProps, mapDispatchToProps)(Presentational);
+
+// 8/10
 
  */
